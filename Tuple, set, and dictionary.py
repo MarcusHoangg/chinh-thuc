781516b901d937ccdfb7ee3a113a997a490a0116
MarcusHoangg <MINH.HOANG4@metropolia.fi>
2024-09-20 10:45:20 +0300

    second commit

--- Tuple, set, and dictionary.py
+++ Tuple, set, and dictionary.py
@@ -26,7 +26,7 @@ dice1,dice2=cast()
 print(f"the dice show is {dice1} and {dice2}")
 '''
 #
-''''''
+'''''
 students={"poon","suzy","sharmila","shorstika"}
 print(type(students))
 print(students)
@@ -37,9 +37,50 @@ print(students)
 students.remove("sailesh")
 print(students)
 '''
-#
-dt1={'suman' : 89,'minh':90,'long':86,'hoang':91,'olga':90
-total=sum(dt1.values())
+#exercise
+#ex1
+'''
+seasons=("WINTER","WINTER","SPRING","SPRING","SUMMER","SUMMER","SUMMER","AUTUMN","AUTUMN","AUTUMN","WINTER")
+month=int(input("Enter the month number: "))
+if 1 <=month<=12:
+    print(f'month is {month} related to {seasons[month-1]}')
+'''
+#ex2
+'''
+name_set=set()
+while True:
+    user_input=input("Enter the name of a person(or press enter to exit): ")
+    if user_input =="":
+        break
+    if user_input  in name_set:
+        print("name existed")
+    else:
+        print(f'new name{user_input}')
+        name_set.add(user_input)
+    print("list of name entered")
+    for nme in name_set:
+        print(name)
+'''
+#ex3
+'''
+airport={}
+command=input("type 'add' to add a new airport,;'view'to find an airport or 'quit' to quit the program :")
+while command !="quit":
+    if command =="add":
+        airport_name=input("Enter the name of the airport: ")
+        airport_ICAO=input("Enter the ICAO code: ").upper()
+        airport_list[airport_ICAO]=airport_name
+    elif command =="view":
+        airport_view=input("Enter the ICAO code of the airport: ")
+        if airport_view in airport_list:
+            print(f'the airport with {airport_view} code is airport{airport_list[airport_view]}'')
+        else:
+            print("airport not found")
+    else:
+            print("invalid command")
+    command = input("type 'add' to add a new airport,;'view'to find an airport or 'quit' to quit the program :")
+'''
+
 
 
 
--- bai tap lam them.py
+++ bai tap lam them.py
@@ -41,6 +41,30 @@ c=6-a-b
 print("c is",c)
 '''
 #chia het cho 3
+'''
 n=int(input("enter your number:"))
 res=n-n%3
 print("the result is",res)
+'''
+
+'''
+operators=input("open a operator (+ - */):")
+num1=float(input("enter your number:"))
+num2=float(input("enter your number:"))
+if operators=="+":
+    result=num1+num2
+    print("the result is",result)
+elif operators=="-":
+    result=num1-num2
+    print("the result is",result)
+elif operators=="*":
+    result=num1*num2
+    print("the result is",result)
+'''
+temp=int(input("enter the temperature:"))
+condition = input('the weather is sunny(True/False):')
+if temp>=29 or condition==True:
+    print("you can go outside")
+else:
+    print("you cannot go outside")
+
